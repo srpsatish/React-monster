d700459d3f8ba817f9464aa79da44e6f673528a8
Satish Singh <singhsatish7275@gmail.com>
2020-07-08 20:23:04 +0530

    fetching with react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters : []
+      // string : "Hi I am srpsatish!"
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users/")
+    .then(response => response.json())
+    .then(users => this.setState({ monsters : users}));
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            this.state.monsters.map(monsters => (
+            <h1 key={monsters.id}>{monsters.name}</h1>
+            ))
+          }
+          {/* <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            {this.state.string}
+          </p>
+          <button onClick= {() => this.setState({ string : "Hi I am Satish Singh!"})}>Change It.</button> */}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
