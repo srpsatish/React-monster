82b74ee58a09bb4b610558245f43f285ee541b68
Satish Singh <singhsatish7275@gmail.com>
2020-07-10 15:26:05 +0530

    Updates

---
+++ precache-manifest.874290c9076b315a56a32455f2be8752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496e8ab2f8fde699f9e7613dbd75a1fd",
+    "url": "/React-monster/index.html"
+  },
+  {
+    "revision": "5618e03e11552cfa79f4",
+    "url": "/React-monster/static/css/main.8393a0c7.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/React-monster/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-monster/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5618e03e11552cfa79f4",
+    "url": "/React-monster/static/js/main.6aed9034.chunk.js"
+  },
+  {
+    "revision": "a3858c5ca76851957032",
+    "url": "/React-monster/static/js/runtime-main.d0033bdb.js"
+  }
+]);
